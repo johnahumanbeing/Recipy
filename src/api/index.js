744276a47847ed0e93069ed6716c86cfc4bc0bae
johnahumanbeing <mseewak@gmail.com>
2024-07-12 10:52:58 +0300

    added the api base url

--- src/api/index.js
+++ src/api/index.js
@@ -3,7 +3,7 @@ import { mealType, mergeDuplicateRecipes } from '../utils';
 
 export const api_key = '446aa5a2b7ad7f4594f12336c100a887';
 export const app_id = '3cec3aee';
-export const baseUrl = `https://api.edamam.com/api/recipes/v2/`;
+export const baseUrl = `https://api.edamam.com/api/recipes/v2/`; // the api base url
 export const credentials = `&app_id=${app_id}&app_key=${api_key}`;
 
 export const getRecipesByMealType = async (meal) => {
